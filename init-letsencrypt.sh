3489a99f50857b34466f6e7dfc7a36779cf9a363
Cameron Clough <cameronjclough@gmail.com>
2022-04-20 19:49:30 +0100

    fix uat domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(uat.api.retropilot.org)
+domains=(api.uat.retropilot.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
