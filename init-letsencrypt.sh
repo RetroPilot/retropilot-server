17c02c84ed0d092c0013498b1c3c3a20530dba3c
Cameron Clough <cameronjclough@gmail.com>
2022-04-20 20:15:06 +0100

    revert

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api.uat.retropilot.org)
+domains=(uat.api.retropilot.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
