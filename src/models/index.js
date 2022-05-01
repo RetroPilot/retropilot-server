7ff8d5f190a2d2067048f2e48af1b884c6c72b2a
AdamSBlack <18472768+AdamSBlack@users.noreply.github.com>
2022-05-01 23:08:16 +0100

    Add settings api, update models

--- src/models/index.js
+++ src/models/index.js
@@ -1,23 +1,15 @@
 import Accounts from './accounts.model';
-import AthenaActionLog from './athena_action_log.model';
-import AthenaReturnedData from './athena_returned_data.model';
-import DeviceAuthorisedUsers from './device_authorised_users.model';
 import Devices from './devices.model';
 import DriveSegments from './drive_segments.model';
 import Drives from './drives.model';
-import OAuthAccounts from './oauth_accounts.model';
 
 import orm from './orm';
 
 export {
   Accounts,
-  AthenaActionLog,
-  AthenaReturnedData,
-  DeviceAuthorisedUsers,
   Devices,
   DriveSegments,
   Drives,
-  OAuthAccounts,
 };
 
 export default orm;
